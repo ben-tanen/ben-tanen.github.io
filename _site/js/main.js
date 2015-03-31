0f1d8ea76013dd0b9b9e3292c4dc40aee7ef315d
Ben Tanen <ben.tanen@gmail.com>
2015-03-31 15:07:26 -0400

    rebuilt _site

---
+++ _site/js/main.js
@@ -0,0 +1,25 @@
+function pageResize() {
+	var window_width = $(window).width();
+	var window_height = $('.side').height();
+
+	// side bar heigh resize
+
+	// main width resize
+	if (window_width - 250 > 800) {
+		$('.main').css('width', 800 + 'px');
+		$('.post img').css
+	} else if (window_width - 250 < 500){
+		$('.main').css('width', 500 + 'px');
+	} else {
+		$('.main').css('width', window_width - 250 + 'px');
+	}
+
+
+	
+}
+
+$(document).ready(function() {
+	pageResize();
+
+	$(window).resize(pageResize);
+});
