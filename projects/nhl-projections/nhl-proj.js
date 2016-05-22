b0917fd0375474c4068e7584448d0334fb4d34bc
Ben Tanen <ben.tanen@gmail.com>
2016-05-22 19:07:28 -0400

    added nhl-projections

---
+++ projects/nhl-projections/nhl-proj.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#nhl-about #expand-img').click(function() {
+        $('#nhl-about').toggleClass('min');
+        $('#nhl-about').toggleClass('expand');
+    });
+});
