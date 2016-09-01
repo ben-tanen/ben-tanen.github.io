079074f7e3608e5ee3f3c7cd1cd63a1772cca435
Ben Tanen <ben.tanen@gmail.com>
2016-08-31 23:31:53 -0400

    (1) updated ASL-LEX link, (2) added remaining nhl-project pictures

--- _site/projects/nhl-projections/nhl-proj.js
+++ _site/projects/nhl-projections/nhl-proj.js
@@ -1,34 +1,34 @@
 var teamLogos = {
     'ANA': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/anaheim-ducks.png',
-    'ARI': null,
+    'ARI': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/arizona-coyotes.png',
     'BOS': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/boston-bruins.png',
-    'BUF': null,
-    'CGY': null,
-    'CAR': null,
+    'BUF': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/buffalo-sabres.png',
+    'CGY': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/calgary-flames.png',
+    'CAR': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/carolina-hurricanes.png',
     'CHI': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/chicago-blackhawks.png',
-    'COL': null,
-    'CBJ': null,
-    'DAL': null,
-    'DET': null,
-    'EDM': null,
+    'COL': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/colorado-avalanche.png',
+    'CBJ': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/columbus-blue-jackets.png',
+    'DAL': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/dallas-stars.png',
+    'DET': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/detroit-red-wings.png',
+    'EDM': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/edmonton-oilers.png',
     'FLA': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/florida-panthers.png',
-    'LAK': null,
-    'MIN': null,
-    'MTL': null,
-    'NSH': null,
-    'NJD': null,
-    'NYI': null,
+    'LAK': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/los-angeles-kings.png',
+    'MIN': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/minnesota-wild.png',
+    'MTL': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/montreal-canadiens.png',
+    'NSH': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/nashville-predators.png',
+    'NJD': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/new-jersey-devils.png',
+    'NYI': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/new-york-islanders.png',
     'NYR': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/new-york-rangers.png',
-    'OTT': null,
-    'PHI': null,
-    'PIT': null,
-    'SJS': null,
-    'STL': null,
-    'TBL': null,
-    'TOR': null,
-    'VAN': null,
+    'OTT': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/ottawa-senators.png',
+    'PHI': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/philadelphia-flyers.png',
+    'PIT': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/pittsburgh-penguins.png',
+    'SJS': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/san-jose-sharks.png',
+    'STL': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/st-louis-blues.png',
+    'TBL': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/tampa-bay-lightning.png',
+    'TOR': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/toronto-maple-leafs.png',
+    'VAN': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/vancouver-canucks.png',
     'WSH': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/washington-capitals.png',
-    'WPG': null,
+    'WPG': 'http://www.sportsnet.ca/wp-content/themes/sportsnet-nhl/images/team_logos/200x200/hockey/nhl/winnipeg-jets.png',
 };
 
 function parseTeamData(line) {
@@ -61,7 +61,7 @@ function updatePoints(data) {
 
     // update table
     for (var i = 0; i < teams.length; i++) {
-        var row_str = '<tr><td><img style="width: 50px; padding: 3px 5px 3px 10px;" />';
+        var row_str = '<tr><td><img style="width: 30px; padding-bottom: 4px; padding-left: 15px;" />';
         for (var j = 0; j < $('#projections_table thead tr:nth-child(1)').children().length - 1; j++) row_str += '<td></td>';
         row_str += '</tr>'; 
         $('#projections_table tbody').append(row_str);
