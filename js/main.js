585454ce75fa81555e98149a3ae661e7b3c84309
Ben Tanen <ben.tanen@gmail.com>
2016-09-29 02:28:53 -0400

    attempted responsive / mobile view fix

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 function pageResize() {
 	var window_width = $(window).width();
-	var window_height = $('.side').height();
 
 	// main width resize
 	if (window_width - 275 > 800) {
-		console.log($('.wrapper').css('margin-left'));
 		$('.main').css('width', 800 + 'px');
 		$('.side').css('left', $('.wrapper').css('margin-left'))
-	} else if (window_width - 275 < 500){
+	} else if (window_width - 275 < 500 || $('.side').width() != 250){
 		$('.main').css('width', 500 + 'px');
 	} else {
 		$('.main').css('width', window_width - 275 + 'px');
