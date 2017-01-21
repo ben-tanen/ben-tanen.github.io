1a2740e6f9e221676b4bdcd67cdc5e211ed19e28
Ben Tanen <ben.tanen@gmail.com>
2017-01-21 10:51:27 -0500

    cleared css junk

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,19 +1,5 @@
 function pageResize() {
 	$('.side').css('left', $('.wrapper').css('margin-left'));
-
-	/*
-	var window_width = $(window).width();
-
-	// main width resize
-	if (window_width - 275 > 800) {
-		$('.main').css('width', 800 + 'px');
-		$('.side').css('left', $('.wrapper').css('margin-left'))
-	} else if (window_width - 275 < 500 || $('.side').width() != 250){
-		$('.main').css('width', 500 + 'px');
-	} else {
-		$('.main').css('width', window_width - 275 + 'px');
-	}
-	*/
 }
 
 $(document).ready(function() {
