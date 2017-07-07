f6cc7c241611c64b938f9aedb49f17fe7c83c9e4
Ben Tanen <ben.tanen@gmail.com>
2017-07-07 03:17:16 -0400

    updated music projects (vinyl + story time)

---
+++ _site/projects/music/vinyl/assets/load-records.js
@@ -0,0 +1,20 @@
+$.ajax({
+    type: "GET",
+    url: "http://bt-dbs.herokuapp.com/getVinylRecords",
+    dataType: "text",
+    success: function(d) {
+        var d = JSON.parse(d);
+
+        for (var i = d.length - 1; i >= 0; i--) {
+            var album     = d[i].album,
+                artist    = d[i].artist,
+                cover_url = d[i].cover_url;
+
+            $('#bt-record-holder').append('<div class="bt-record"><img src="' + cover_url + '" /><p><span style="color: #77bdee">' + album + '</span><br />' + artist + '</p></div>');
+        }
+
+        setTimeout(function() {
+            $('#bt-record-holder').animate({opacity: 1}, 500);
+        }, 500);
+    }
+});
