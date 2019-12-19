277fd5c7a05a24a804d0b8e83f5f5c15ad3abe46
Ben Tanen <ben.tanen@gmail.com>
2019-12-19 16:18:53 -0500

    finished how-bills-die project (first draft probably...)

--- projects/how-bills-die/js/main.js
+++ projects/how-bills-die/js/main.js
@@ -1,11 +1,3 @@
-/* 
-TO DO:
-4. make x scale unique for house vs. senate?
-5. to include resolving differences/president bills?
-6. add tooltips? to axis labels to explain meaning; to bars?
-7. add data labels
-8. make vote bucket all blue
-*/
 
 /*********************/
 /*** INIT VARIABLE ***/
@@ -13,8 +5,8 @@ TO DO:
 
 let hbd_svg = d3.select('#d3-how-bills-die');
 
-let hbd_margin = {top: 30, right: 20, bottom: 10, left: 225, middle_x: 100, middle_y: 10},
-    hbd_width  = $('#d3-how-bills-die').width() - hbd_margin.left - hbd_margin.right - hbd_margin.middle_x,
+let hbd_margin = {top: 30, right: 30, bottom: 10, left: 170, middle_x: 75, middle_y: 10},
+    hbd_width  = $('#scroll-container').width() - hbd_margin.left - hbd_margin.right - hbd_margin.middle_x,
     hbd_height = $('#d3-how-bills-die').height() - hbd_margin.top - hbd_margin.bottom - hbd_margin.middle_y * 4;
 
 let data = [ ];
@@ -31,33 +23,6 @@ let x = d3.scaleLinear().range([0, hbd_width / 2]),
 /********************************/
 /*** DECLARE HELPER FUNCTIONS ***/
 /********************************/
-
-function wrap(text) {
-    text.each(function() {
-        var text = d3.select(this),
-            words = text.text().split(/\s+/).reverse(),
-            word,
-            line = [],
-            lineNumber = 0,
-            lineHeight = 1.1, // ems
-            x = text.attr("x"),
-            y = text.attr("y"),
-            ww = 100,
-            dy = 0,
-            tspan = text.text(null).append("tspan").attr("x", x).attr("y", y).attr("dy", dy + "em");
-            console.log(x)
-        while (word = words.pop()) {
-            line.push(word);
-            tspan.text(line.join(" "));
-            if (tspan.node().getComputedTextLength() > ww) {
-                line.pop();
-                tspan.text(line.join(" "));
-                line = [word];
-                tspan = text.append("tspan").attr("x", x).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
-            }
-        }
-    });
-}
 
 function init_bars(date) {
     hbd_svg.selectAll("rect.bar.back")
@@ -96,7 +61,7 @@ function init_bars(date) {
         .classed("invisible", (d) => d.N == 0)
         .attr("x", (d) => hbd_margin.left + (d.chamber_curr === "hr" ? 0 : hbd_margin.middle_x) + (hbd_width / 2) - (d.chamber_curr === "hr" ? 1 : -1) * (x(d.N) + 2))
         .attr("y", (d) => hbd_margin.top + y(d.status_bucket) + (hbd_height / n_buckets) / 2)
-        .text((d) => d.N_curr + " bills (" + (d.N_curr / d.N_chamber * 100).toFixed(1) + "%)" + (d.chamber_curr === "hr" & d.status_bucket === "A. INTRODUCTION" ? " are still here" : ""));
+        .text((d) => d.N_curr + " bills (" + (d.N_curr / d.N_chamber * 100).toFixed(1) + "%)" + (d.chamber_curr === "hr" & d.status_bucket === "A. INTRODUCED" ? " are still here" : ""));
 
     labels.selectAll("tspan.lower-label")
         .data((d) => [d]).enter()
@@ -106,7 +71,7 @@ function init_bars(date) {
         .attr("x", (d) => hbd_margin.left + (d.chamber_curr === "hr" ? 0 : hbd_margin.middle_x) + (hbd_width / 2) - (d.chamber_curr === "hr" ? 1 : -1) * (x(d.N) + 2))
         .attr("y", (d) => hbd_margin.top + y(d.status_bucket) + (hbd_height / n_buckets) / 2)
         .attr("dy", "14px")
-        .text((d) => (d.N - d.N_curr) + " bills" + (d.chamber_curr === "hr" & d.status_bucket === "A. INTRODUCTION" ? " have progressed" : ""));
+        .text((d) => (d.N - d.N_curr) + " bills (" + ((d.N - d.N_curr) / d.N_chamber * 100).toFixed(1) + "%)" + (d.chamber_curr === "hr" & d.status_bucket === "A. INTRODUCED" ? " have moved on" : ""));
 }
 
 function update_bars(date, duration = 0) {
@@ -130,14 +95,14 @@ function update_bars(date, duration = 0) {
         .classed("invisible", (d) => d.N == 0)
         .transition().duration(duration)
         .attr("x", (d) => hbd_margin.left + (d.chamber_curr === "hr" ? 0 : hbd_margin.middle_x) + (hbd_width / 2) - (d.chamber_curr === "hr" ? 1 : -1) * (x(d.N) + 2))
-        .text((d) => d.N_curr + " bills (" + (d.N_curr / d.N_chamber * 100).toFixed(1) + "%)" + (d.chamber_curr === "hr" & d.status_bucket === "A. INTRODUCTION" ? " are still here" : ""))
+        .text((d) => d.N_curr + " bills (" + (d.N_curr / d.N_chamber * 100).toFixed(1) + "%)" + (d.chamber_curr === "hr" & d.status_bucket === "A. INTRODUCED" ? " are still here" : ""))
 
     labels.selectAll("tspan.lower-label")
         .data((d) => [d])
         .classed("invisible", (d) => d.N == 0)
         .transition().duration(duration)
         .attr("x", (d) => hbd_margin.left + (d.chamber_curr === "hr" ? 0 : hbd_margin.middle_x) + (hbd_width / 2) - (d.chamber_curr === "hr" ? 1 : -1) * (x(d.N) + 2))
-        .text((d) => (d.N - d.N_curr) + " bills" + (d.chamber_curr === "hr" & d.status_bucket === "A. INTRODUCTION" ? " have progressed" : ""));
+        .text((d) => (d.N - d.N_curr) + " bills (" + ((d.N - d.N_curr) / d.N_chamber * 100).toFixed(1) + "%)" + (d.chamber_curr === "hr" & d.status_bucket === "A. INTRODUCED" ? " have moved on" : ""));
 }
 
 function draw_bucket_labels() {
@@ -147,7 +112,7 @@ function draw_bucket_labels() {
         .classed("bucket-label", true)
         .attr("x", hbd_margin.left + hbd_width / 2 + hbd_margin.middle_x / 2)
         .attr("y", (d) => hbd_margin.top + y(d) + (hbd_height / n_buckets) / 2)
-        .text((d) => d);
+        .text((d) => d.substr(3));
 }
 
 function draw_chamber_labels() {
@@ -164,17 +129,6 @@ function draw_chamber_labels() {
         .attr("x", hbd_margin.left + hbd_margin.middle_x + hbd_width * 0.75)
         .attr("y", hbd_margin.top * 0.5)
         .text("Senate");
-}
-
-function draw_date_axis_labels() {
-    let d3_container_height = $('#d3-how-bills-die').parent().parent().height();
-    for (let i = 1; i <= n_scroll_stops; i++) {
-        d3.select("#scroll-viz-container").append("text")
-            .classed("date-axis-label", true)
-            .attr("id", "label-" + date_to_input_str(date(i)))
-            .style("top", ((i - 1) * (d3_container_height / n_scroll_stops) + hbd_height / 2 + hbd_margin.top) + "px")
-            .text(date_to_input_str(date(i)));
-    }
 }
 
 function date_to_input_str(date) {
@@ -233,12 +187,11 @@ d3.csv("data/2019-10-21_bill-counts-by-day-long.csv", (d) => {
     // adjust scale domain based on data
     x.domain(d3.extent(data, (d) => d.N));
     y.domain(d3.map(data.filter((d) => d.chamber_curr != "b"), (d) => d.status_bucket).keys().sort());
-    date.range(d3.extent(data, (d) => new Date(d.date + " ")));
+    date.range(d3.extent(data, (d) => new Date(d.date)));
     curr_date = d3.min(data, (d) => d.date);
 
     draw_chamber_labels();
     draw_bucket_labels();
-    draw_date_axis_labels();
     init_bars("2019-01-03");
 })
 
@@ -247,6 +200,8 @@ d3.csv("data/2019-10-21_bill-counts-by-day-long.csv", (d) => {
 /*********************************/
 
 $(document).ready(() => {
+    let hbd_width  = $('#d3-how-bills-die').width() - hbd_margin.left - hbd_margin.right - hbd_margin.middle_x,
+        hbd_height = $('#d3-how-bills-die').height() - hbd_margin.top - hbd_margin.bottom - hbd_margin.middle_y * 4;
     sticky_scroll();
 });
 
@@ -274,17 +229,17 @@ $(window).resize(() => {
 
 $(window).scroll(() => {
 
-    screen_margin = ($(window).height() - $('#d3-how-bills-die').height()) / 2,
-    d3_container_top = $('#d3-how-bills-die').parent().offset().top,
-    d3_container_height = $('#d3-how-bills-die').parent().parent().height();
+    let date_ix = $("p.date-axis-label").map(function() { 
+        let t = $(this).offset().top; 
+        return t < ($(window).scrollTop() + $(window).height() * 0.5) ? t : null; 
+    }).length;
 
-    date_ix = Math.ceil(Math.max(1, $(window).scrollTop() - (d3_container_top - screen_margin)) / (d3_container_height / n_scroll_stops));
-    if (date_ix >= 1) {
+    if (date_ix > 0) {
         if (date_to_input_str(date(date_ix)) != curr_date) {
             curr_date = date_to_input_str(date(date_ix))
             update_bars(curr_date, 250);
-            d3.selectAll("text.date-axis-label").classed('focus', false);
-            d3.select("text.date-axis-label#label-" + curr_date).classed('focus', true);
+            d3.selectAll("p.date-axis-label").classed('focus', false);
+            d3.select("p.date-axis-label#label-" + curr_date).classed('focus', true);
         }
     }
 
